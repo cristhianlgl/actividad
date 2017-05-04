57deccfafc2978cf91c0413c23471660267555ca
cristhianlgl <cristhianlgl@hotmail.com>
2017-05-04 00:50:24 -0500

    cambios

--- actividad_1.cpp
+++ actividad_1.cpp
@@ -8,22 +8,39 @@ int main()
     while(opcion != 4)
     {
         system("cls");
-        printf("Programa para gestionar pilas con números enteros \n \n");
-        printf("MENU DE OPCIONES \n \n");
-        printf("1. Insertar pila (inserta un número entero en la pila) \n");
-        printf("2. Borrar en pila (borra el último número de la pila) \n");
+        printf("        PROGRAMA PARA GESTIONAR PILAS CON NUMERO ENTEROS \n \n");
+        printf("-----------------------MENU DE OPCIONES-------------------- \n \n");
+        printf("1. Insertar pila (inserta un numero entero en la pila) \n");
+        printf("2. Borrar en pila (borra el ultimo numero de la pila) \n");
         printf("3. Listar pila (imprime en pantalla la pila actual) \n");
-        printf("4. Salir (sale del programa) \n");
-        printf("Por favor digite una opcion: \n");
+        printf("4. Salir (sale del programa) \n\n");
+        printf("Por favor digite una opcion: ");
         scanf("%d",&opcion);
 
         switch(opcion)
         {
-        case 1:
-            system("cls");
-            printf("Hola opcion 1\n");
-            system("pause");
-            break;
+            case 1:
+                system("cls");
+                printf("INSERTAR PILA \n\n");
+                system("pause");
+                break;
+            case 2:
+                system("cls");
+                printf("BORRAR PILA \n\n");
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                printf("LISTAR PILA \n\n");
+                system("pause");
+                break;
+            case 4:
+                break;
+            default:
+                system("cls");
+                printf("La opcion digitada no es correcta \n");
+                system("pause");
+                break;
         }
 
     }
